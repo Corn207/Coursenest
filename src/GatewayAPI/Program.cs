abcfe9ab5014b35613774f2dd0de7fa32d05646d
Corn207 <halowars2000@gmail.com>
2023-02-16 00:11:02 +0700

    Added CORS to GatewayAPI.

--- src/GatewayAPI/Program.cs
+++ src/GatewayAPI/Program.cs
@@ -3,7 +3,17 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddReverseProxy()
 	.LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
 
+builder.Services.AddCors(options =>
+{
+	options.AddDefaultPolicy(configure =>
+	{
+		configure.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod();
+	});
+});
+
 var app = builder.Build();
+
+app.UseCors();
 
 app.MapReverseProxy();
 
