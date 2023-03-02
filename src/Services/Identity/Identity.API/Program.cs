3b5c6a0d8cc45b78343cece97fc4770971cdd587
Corn207 <halowars2000@gmail.com>
2023-03-03 02:57:30 +0700

    Modified Program.cs to use SwaggerUI based on Env args SwaggerUI=True. Modified docker compose files.

--- src/Services/Identity/Identity.API/Program.cs
+++ src/Services/Identity/Identity.API/Program.cs
@@ -9,11 +9,7 @@ builder.Services.AddDefaultServices<DataContext>(builder.Configuration);
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-	app.UseSwagger();
-	app.UseSwaggerUI();
-}
+app.UseSwaggerUI();
 
 app.UseAuthentication();
 
